da0b9e012e152f5b0bf2d0001d49d7f2027bf81b
gyoomi <gyoomi0709@foxmail.com>
2019-03-06 16:05:36 +0800

    feat1:add do 2

---
+++ src/main/java/com/git/core/feat01/Do2.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.git.core.feat01;
+
+/**
+ * do2
+ *
+ * @author Leon
+ * @version 2019/3/6 16:05
+ */
+public class Do2 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(222);
+    }
+}
