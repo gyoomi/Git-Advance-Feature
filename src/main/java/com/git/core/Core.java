ea82b532a5467c45d68278be89e6a3a601cf8211
gyoomi <gyoomi0709@foxmail.com>
2019-03-06 15:51:38 +0800

    Initial commit

---
+++ src/main/java/com/git/core/Core.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.git.core;
+
+/**
+ * 基础核心类
+ *
+ * @author Leon
+ * @version 2019/3/6 15:50
+ */
+public class Core {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
