bf7ad3e252d0c521ab799e6a7a93efac7da8f09c
gyoomi <gyoomi0709@foxmail.com>
2019-03-06 15:57:41 +0800

    feat1:add for core

---
+++ src/main/java/com/git/core/feat01/Core.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright © 2019, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.git.core.feat01;
+
+/**
+ * 功能开发-1 核心
+ *
+ * @author Leon
+ * @version 2019/3/6 15:56
+ */
+public class Core {
+
+    public static void main(String[] args) {
+        System.out.println("feat1111");
+    }
+}
