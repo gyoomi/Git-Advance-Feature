92a3b6740656359c26715a97516db2665a662f7c
leon <gyoomi0709@foxmail.com>
2019-03-07 20:56:29 +0800

    合并提交1-2后的总的提交

---
+++ src/main/java/com/git/core/feat02/Do01.java
@@ -0,0 +1,14 @@
+package com.git.core.feat02;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/3/7 20:56
+ */
+public class Do01 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(222222);
+    }
+}
---
+++ src/main/java/com/git/core/feat02/Do02.java
@@ -0,0 +1,18 @@
+package com.git.core.feat02;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2019/3/7 20:56
+ */
+public class Do02 {
+
+    public static void main(String[] args) throws Exception {
+        test();
+    }
+
+    public static void test() {
+        System.out.println(1111111);
+    }
+}
